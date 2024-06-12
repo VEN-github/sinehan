19c4e2c27b80a96fb00cc9cc4da243af527209ca
Raven Barrogo <barrogoraven@gmail.com>
2024-06-12 12:15:19 +0800

    ADD: tailwindcss :package:

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint'],
+  modules: ['@nuxt/eslint', '@nuxtjs/tailwindcss'],
   eslint: {
     checker: true,
     config: {
