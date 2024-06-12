6a56c30c1d436216d95be834cf9aa2f2c2c52f39
Raven Barrogo <barrogoraven@gmail.com>
2024-06-12 12:05:59 +0800

    ADD: eslint :shirt:

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['@nuxt/eslint'],
+  eslint: {
+    checker: true,
+    config: {
+      stylistic: true,
+    },
+  },
 })
