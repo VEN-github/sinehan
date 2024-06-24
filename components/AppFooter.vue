e15f35f5b62c76b2f55a704e64bddfb5d5970dc9
Raven Barrogo <barrogoraven@gmail.com>
2024-06-24 12:25:00 +0800

    CHANGE: footer :art:

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -4,11 +4,11 @@
       <div class="flex items-center justify-center gap-x-1 text-custom-foreground">
         <IconCopyright :size="18" />
         <p class="text-sm sm:text-lg">
-          2024&nbsp;
+          2024
           <NuxtLink to="/" class="font-medium tracking-tighter text-custom-primary sm:text-xl">
-            sinehan
+            sinehan.
           </NuxtLink>
-          . All rights reserved.
+          All rights reserved.
         </p>
       </div>
     </BaseContainer>
